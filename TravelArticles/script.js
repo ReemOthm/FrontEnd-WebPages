bed7105b546266387935e3bff1e45af75c29aa38
Reem <reemoth@outlook.com>
2023-10-22 06:23:19 +0300

    Travel Articles Blog Page Designed By Me

---
+++ TravelArticles/script.js
@@ -0,0 +1,7 @@
+let articles=[];
+console.log(Node);
+
+let date = new Date();
+
+let copyRight = document.getElementById('copyright');
+copyRight.innerHTML = `${copyRight.innerHTML}  ${date.getFullYear()}`;
